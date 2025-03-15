737ceecb1d95d09c79d22e700ff06a3057f1bdbd
Yahya Jawad <ifelixridge007@gmail.com>
2025-03-15 10:54:22 +0300

    changing scan library for better performance

--- lib/features/home/presentation/views/widgets/addPlant/addPlantScanViewBody.dart
+++ lib/features/home/presentation/views/widgets/addPlant/addPlantScanViewBody.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:go_router/go_router.dart';
-import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
+import 'package:riwaa/core/components/scanRect.dart';
 import 'package:riwaa/core/utilities/appAssets.dart';
 import 'package:riwaa/core/utilities/appRouter.dart';
 import 'package:riwaa/core/utilities/appStyles.dart';
 import 'package:riwaa/core/utilities/constants.dart';
 import 'package:riwaa/features/home/presentation/views/widgets/addPlant/addPlantAppBar.dart';
-
 
 class AddPlantScanViewBody extends StatefulWidget {
   const AddPlantScanViewBody({super.key});
@@ -17,75 +17,74 @@ class AddPlantScanViewBody extends StatefulWidget {
 }
 
 class _AddPlantScanViewBodyState extends State<AddPlantScanViewBody> {
-  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
-  QRViewController? controller;
+  final MobileScannerController controller = MobileScannerController(
+    formats: [BarcodeFormat.qrCode], // Restrict to QR codes only
+    detectionSpeed: DetectionSpeed.noDuplicates, // Balance between speed and resource usage
+    facing: CameraFacing.back, // Use back camera
+    torchEnabled: false, // Flash off by default
+  );
 
   @override
   void dispose() {
-    controller?.pauseCamera();
-    controller?.dispose();
+    controller.dispose();
     super.dispose();
-  }
-  @override
-  void reassemble() {
-    super.reassemble();
-    controller?.dispose();
-    controller = null;
   }
 
   bool isValidHash(String hash) {
+    print(hash);
     return hash.length == 37 && hash.startsWith("riwaa");
   }
-  void _onQRViewCreated(QRViewController controller) {
-    
-    this.controller = controller;
-    controller.scannedDataStream.listen((scanData) {
-      if (scanData.code != null) {
-        controller.pauseCamera();
-        // ignore: use_build_context_synchronously
-        GoRouter.of(context).pushReplacement(
-          AppRouter.addPlant,
-          extra: isValidHash(scanData.code!) ? scanData.code!.substring(5) : "wrong"
-        );
-      }
-    });
-  }
-
 
   @override
   Widget build(BuildContext context) {
     return Stack(
-        children: [
-          // QR Scanner with no overlay
-          QRView(
-            key: qrKey,
-            onQRViewCreated: _onQRViewCreated,
-            overlay: null, // No built-in overlay
-          ),
-          
-          const AddPlantAppBar(),
-          // Custom SVG overlay
-          Opacity(
-            opacity: 0.65,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SvgPicture.asset(
-                  AppAssets.qrcode,
-                  width: MediaQuery.sizeOf(context).width
+      children: [
+        // QR Scanner with no overlay
+        MobileScanner(
+          controller: controller,
+          scanWindow: Rect.largest,
+          onDetect: (capture) {
+            final List<Barcode> barcodes = capture.barcodes;
+            for (final barcode in barcodes) {
+              if (barcode.rawValue != null) {
+                controller.stop();
+                // Navigate to the next screen
+                GoRouter.of(context).pushReplacement(
+                  AppRouter.addPlant,
+                  extra: isValidHash(barcode.rawValue!) 
+                      ? barcode.rawValue!.substring(5) 
+                      : "wrong"
+                );
+                break;
+              }
+            }
+          },
+        ),
+        
+        // App Bar
+        const AddPlantAppBar(),
+        
+        // Custom SVG overlay
+        Opacity(
+          opacity: 0.65,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SvgPicture.asset(
+                AppAssets.qrcode,
+                width: MediaQuery.sizeOf(context).width
+              ),
+              Text(
+                'صور الـQR Code الموجود على علبة جهاز رِواء',
+                style: AppStyles.paragraphMedium.copyWith(
+                  color: kPrimaryColor,
+                  fontWeight: FontWeight.w600
                 ),
-                Text(
-                  'صور الـQR Code الموجود على علبة جهاز رِواء',
-                  style: AppStyles.paragraphMedium.copyWith(
-                    color: kPrimaryColor,
-                    fontWeight: FontWeight.w600
-                  ),
-                )
-              ],
-            ),
-          ),   
-        ],
-      );
+              )
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
-
